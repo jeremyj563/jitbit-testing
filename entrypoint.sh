4b024dd0cbe689ea28838d56fb95a2956a150d54
Jeremy Johnson <jeremy.m.johnson@davenportiowa.com>
2021-12-21 14:59:47 -0600

    minor comment update in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# start SQL Server, run the script to create the DB schema, run the app
+# run the script to create the DB schema, start MSSQL, run the app
 /usr/local/bin/init-db.sh & /opt/mssql/bin/sqlservr & cd /var/www/helpdesk && dotnet HelpDesk.dll
